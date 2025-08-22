60d3d0f17c72c9daab9b796af9e44b1de4696fb0
SistemasSinsetec <Practicas@sinsetec.com.mx>
2025-08-22 09:51:07 -0600

    soluccion del problema de angular

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,6 +1,6 @@
+import { provideServerRendering } from '@angular/ssr';
 // src/app/app.config.server.ts
 import { mergeApplicationConfig } from '@angular/core';
-import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
 
 export const serverConfig = mergeApplicationConfig(appConfig, {
--- src/main.server.ts
+++ src/main.server.ts
@@ -1,6 +1,6 @@
+import { provideServerRendering } from '@angular/ssr';
 // src/main.server.ts
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideServerRendering } from '@angular/platform-server';
 import { mergeApplicationConfig } from '@angular/core';
 import { serverConfig } from './app/app.config.server';
 import { LoginComponent } from './app/auth/components/login/login.component';
